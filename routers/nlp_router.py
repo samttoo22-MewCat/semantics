7072f6dd1a7d5363388087fc64d28905da78ca63
Yuuko <aznrogerazn@gmail.com>
2024-03-30 09:10:35 +0800

    feat(): mapping more services to router

--- routers/nlp_router.py
+++ routers/nlp_router.py
@@ -7,16 +7,37 @@ from hanlp_common import document
 router = APIRouter()
 nlp_service = NLPService()
 
-# Named Entity Recognition
 @router.post("/ner", response_model=NLPResponse)
 async def perform_nlp_task(request: NLPRequest):
-  return await nlp_service.parse_ner(request.text)
-  # return NLPResponse(result=result)
+  result = await nlp_service.parse_ner(request.text)
+  return NLPResponse(result=result)
 
 @router.post("/tokenise", response_model=NLPResponse)
 async def perform_tokenise(request: NLPRequest):
-  return await nlp_service.tokenise(request.text)
+  result = await nlp_service.tokenise(request.text)
+  return NLPResponse(result=result)
 
 @router.post("/amr-en", response_model=NLPResponse)
 async def perform_tokenise(request: NLPRequest):
-  return await nlp_service.parse_amr_en(request.text)
+  result = await nlp_service.parse_amr_en(request.text)
+  return NLPResponse(result=result)
+
+@router.post("/con", response_model=NLPResponse)
+async def perform_con(request: NLPRequest):
+  result = await nlp_service.con(request.text)
+  return NLPResponse(result=result)
+
+@router.post("/dep", response_model=NLPResponse)
+async def perform_dep(request: NLPRequest):
+  result = await nlp_service.dep(request.text)
+  return NLPResponse(result=result)
+
+@router.post("/sdp", response_model=NLPResponse)
+async def perform_sdp(request: NLPRequest):
+  result = await nlp_service.sdp(request.text)
+  return NLPResponse(result=result)
+
+@router.post("/ud", response_model=NLPResponse)
+async def perform_ud(request: NLPRequest):
+  result = await nlp_service.ud(request.text)
+  return NLPResponse(result=result)
